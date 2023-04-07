bf1a5c46c8764e2a39a59c996ed7a323e2cb90d3
Dmitry Sinkevich <Chameleon1312@yandex.com>
2023-04-07 18:10:29 +0300

    test_push

--- api_yamdb/api/apps.py
+++ api_yamdb/api/apps.py
@@ -1,6 +1,9 @@
+"""Config module."""
 from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
+    """ApiConfig class."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from rest_framework import serializers
 
-from reviews.models import (Review, Comment, Category, User, Genre, Title)
+from reviews.models import (Category, Comment, Genre, Review, Title, User)
 
 
 class ReviewSerializer(serializers.ModelSerializer):
